148f37d89e23a66b7dad1bab8a9ba740446408de
ELMHARMACHE NASSIM <nassim.nassim.raja@gmail.com>
2023-07-27 15:34:16 +0000

    add commit

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,9 +1,89 @@
 import React from "react";
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+import { Carousel } from "react-responsive-carousel";
+import "./_home.scss";
+import img1 from "../../assets/images/img1.jpg";
+import img2 from "../../assets/images/img2.jpg";
+import img3 from "../../assets/images/img3.jpg";
+import img4 from "../../assets/images/img4.jpg";
+import prd1 from "../../assets/images/prd1.jpg";
+import prd3 from "../../assets/images/prd3.jpg";
+import prd4 from "../../assets/images/prd4.jpg";
 
- const Home = () => {
+import { NavLink } from "react-router-dom";
+import TabsProduct from "./components/TabsProduct";
+
+const Home = () => {
   return (
     <>
-    
+      <Carousel className="slides" infiniteLoop="true">
+        <div className="slide">
+          <img src={img1} alt="" srcSet="" />
+          <div className="info">
+            <p className="title">Leather bags</p>
+            <p className="title-two">New Collection</p>
+            <NavLink to={"/"} className="link-shop">
+              Shop Now
+            </NavLink>
+          </div>
+        </div>
+        <div className="slide">
+          <img src={img2} alt="" srcSet="" />
+          <div className="info">
+            <p className="title">Leather bags</p>
+            <p className="title-two">New Collection</p>
+            <NavLink to={"/"} className="link-shop">
+              Shop Now
+            </NavLink>
+          </div>
+        </div>
+        <div className="slide">
+          <img src={img3} alt="" srcSet="" />
+          <div className="info">
+            <p className="title">Leather bags</p>
+            <p className="title-two">New Collection</p>
+            <NavLink to={"/"} className="link-shop">
+              Shop Now
+            </NavLink>
+          </div>
+        </div>
+        <div className="slide">
+          <img src={img4} alt="" srcSet="" />
+          <div className="info">
+            <p className="title">Leather bags</p>
+            <p className="title-two">New Collection</p>
+            <NavLink to={"/"} className="link-shop">
+              Shop Now
+            </NavLink>
+          </div>
+        </div>
+      </Carousel>
+      <div className="coming-soon">
+        <div className="container">
+          <div className="card">
+            <img src={prd1} alt="" />
+            <span>Comming soon</span>
+          </div>
+          <div className="card">
+            <img src={prd4} alt="" />
+            <span>Comming soon</span>
+          </div>
+          <div className="card">
+            <img src={prd3} alt="" />
+            <span>Comming soon</span>
+          </div>
+        </div>
+      </div>
+      <div className="our-product">
+      <div className="heading">
+        <h1>
+         Our Products
+        </h1>
+      </div>
+        <div className="container">
+        <TabsProduct />
+        </div>
+      </div>
     </>
   );
 };
---
+++ src/pages/home/components/TabsProduct.js
@@ -0,0 +1,108 @@
+import React from "react";
+import { Tab, Tabs, TabList, TabPanel } from "react-tabs";
+import "react-tabs/style/react-tabs.css";
+import prd1 from "../../../assets/images/prd1.jpg";
+import prd3 from "../../../assets/images/prd3.jpg";
+
+import "./_tabs.scss";
+const TabsProduct = () => {
+  return (
+    <Tabs className="tabs">
+      <TabList>
+        <Tab>Best Seller</Tab>
+        <Tab>New</Tab>
+        <Tab>Sale</Tab>
+        <Tab>Old</Tab>
+      </TabList>
+
+      <TabPanel>
+        <div className="content">
+          <div className="prj">
+            <div className="image-prj">
+              <img src={prd1} alt="" />
+            </div>
+            <div className="info-prj">
+              <h1>hello Lorem, ipsum.</h1>
+              <p>75.00 $</p>
+            </div>
+            <span>New</span>
+          </div>
+          <div className="prj">
+            <div className="image-prj">
+              <img src={prd1} alt="" />
+            </div>
+            <div className="info-prj">
+              <h1>hello Lorem, ipsum.</h1>
+              <p>75.00 $</p>
+            </div>
+            <span>New</span>
+          </div>
+          <div className="prj">
+            <div className="image-prj">
+              <img src={prd3} alt="" />
+            </div>
+            <div className="info-prj">
+              <h1>hello Lorem, ipsum.</h1>
+              <p>75.00 $</p>
+            </div>
+            <span>New</span>
+          </div>
+          <div className="prj">
+            <div className="image-prj">
+              <img src={prd3} alt="" />
+            </div>
+            <div className="info-prj">
+              <h1>hello Lorem, ipsum.</h1>
+              <p>75.00 $</p>
+            </div>
+            <span>New</span>
+          </div>
+        </div>
+      </TabPanel>
+      <TabPanel>
+        <div className="content">
+          <div className="prj">
+            <div className="image-prj">
+              <img src={prd3} alt="" />
+            </div>
+            <div className="info-prj">
+              <h1>hello Lorem, ipsum.</h1>
+              <p>75.00 $</p>
+            </div>
+            <span>New</span>
+          </div>
+        </div>
+      </TabPanel>
+      <TabPanel>
+        <div className="content">
+          <div className="prj">
+            <div className="image-prj">
+              <img src={prd1} alt="" />
+            </div>
+            <div className="info-prj">
+              <h1>hello Lorem, ipsum.</h1>
+              <p>75.00 $</p>
+            </div>
+            <span>Sale</span>
+          </div>
+        </div>
+      </TabPanel>
+      <TabPanel>
+        <div className="content">
+          <div className="prj">
+            <div className="image-prj">
+              <img src={prd3} alt="" />
+            </div>
+            <div className="info-prj">
+              <h1>hello Lorem, ipsum.</h1>
+              <p>75.00 $</p>
+            </div>
+            <span>Old</span>
+          </div>
+        </div>
+      </TabPanel>
+    </Tabs>
+  );
+};
+
+export default TabsProduct;
